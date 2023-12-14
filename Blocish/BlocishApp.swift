ab5224eaf0fc83c38026bc8f173f482c0b7b083f
Bart Cone <bartcone@outlook.com>
2023-12-14 14:41:09 -0500

    Initial Commit

---
+++ Blocish/BlocishApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocishApp.swift
+//  Blocish
+//
+//  Created by Bart Cone on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BlocishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
